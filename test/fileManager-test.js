f91d2ccafa6c5383dff059566e3082bcba09d9d8
ankihg <hagelin.annika@gmail.com>
2016-03-05 14:58:04 -0800

    gulpfile setup and testing fix

--- test/fileManager-test.js
+++ test/fileManager-test.js
@@ -1,11 +1,12 @@
 'use strict';
-const expect = require('chai');
-const fileManager = require('fileManager');
+const expect = require('chai').expect;
+// const fileManager = require('fileManager');
 
 describe('testing for file manager', function() {
   it('should return data from file', function(done) {
-    var rPath = '/media/palette-bitmap.bmp';
+    // var rPath = '/media/palette-bitmap.bmp';
     // expect(fileManager.read(rPath));
+    expect(true).eql(false);
     done();
   });
 });
