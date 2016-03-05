cc3f280842091cfc17a52956068a876e9425a81e
ankihg <hagelin.annika@gmail.com>
2016-03-04 22:17:02 -0800

    reads file and makes some of bitmap obj

---
+++ test/fileManager-test.js
@@ -0,0 +1,11 @@
+'use strict';
+const expect = require('chai');
+const fileManager = require('fileManager');
+
+describe('testing for file manager', function() {
+  it('should return data from file', function(done) {
+    var rPath = '/media/palette-bitmap.bmp';
+    // expect(fileManager.read(rPath));
+    done();
+  });
+});
